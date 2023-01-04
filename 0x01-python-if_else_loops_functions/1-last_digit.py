624a98b0afa812391a4331a525dd6014725e3032
MOhammed Zakaria <kokoriquaye@gmail.com>
2023-01-04 17:52:29 +0000

    fix

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,7 +5,6 @@ text = "Last digit of"
 mod = abs(number) % 10
 if number < 0:
     mod = mod * -1
-print(mod)
 if mod > 5 and number > 0:
     print("{} {} is {} and is greater than 5".format(text, number, mod))
 elif mod == 0:
