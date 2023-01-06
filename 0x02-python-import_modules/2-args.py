f860754035952c47e1ef674f127deef2b7b4d2c2
MOhammed Zakaria <kokoriquaye@gmail.com>
2023-01-06 22:12:40 +0000

    arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
         for i in range(1, len(argv)):
             print("{}: {}".format(i, argv[i]))
     else:
-        print("{} arguments".format(len(argv) - 1))
+        print("{} arguments:".format(len(argv) - 1))
         for i in range(1, len(argv)):
             print("{}: {}".format(i, argv[i]))
