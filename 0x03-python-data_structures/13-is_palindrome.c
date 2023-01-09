4a29745f2a0f3e96631f4ddc7f64016eb4d13bc4
MOhammed Zakaria <kokoriquaye@gmail.com>
2023-01-09 15:25:05 +0000

    code efficiency fix

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -12,35 +12,33 @@
 
 int is_palindrome(listint_t **head)
 {
-int i = -1, n = 0;
-int *arr1, *arr2;
-const listint_t *current, *new;
+	int i = -1, n = 0, j = 0;
+	int *arr1;
+	const listint_t *current, *new;
 
-current = *head;
-new = *head;
-while (current != NULL)
-{
-current = current->next;
-n++;
-}
-arr1 = malloc(sizeof(arr1) * n);
-arr2 = malloc(sizeof(arr2) * n);
+	current = *head;
+	new = *head;
+	while (current != NULL)
+	{
+		current = current->next;
+		n++;
+	}
+	arr1 = malloc(sizeof(arr1) * n);
+	if (arr1 == NULL)
+		return (1);
 
-if (arr1 == NULL || arr2 == NULL)
-return (1);
-while (new != NULL)
-{
-i++;
-n--;
-arr1[i] = new->n;
-arr2[n] = new->n;
-new = new->next;
-}
-while (i >= 0)
-{
-if (arr1[i] != arr2[i])
-return (0);
-i--;
-}
-return (1);
+	while (new != NULL)
+	{
+		i++;
+		arr1[i] = new->n;
+		new = new->next;
+	}
+	while (i >= 0)
+	{
+		if (arr1[i] != arr1[j])
+			return (0);
+		i--;
+		j++;
+	}
+	return (1);
 }
