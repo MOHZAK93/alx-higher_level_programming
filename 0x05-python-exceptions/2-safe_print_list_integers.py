9ce632cc55172756e3e77ae08db8cce465fbc21e
MOhammed Zakaria <kokoriquaye@gmail.com>
2023-01-23 23:01:16 +0000

    prints the firts x elements of a list and only integers

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
+
 def safe_print_list_integers(my_list=[], x=0):
-    i = 0
+    c = 0
     for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end="")
         except (ValueError, TypeError):
             continue
         else:
-            i += 1
+            c += 1
     print()
-    return (i)
+    return c
