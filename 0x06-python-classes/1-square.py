67a0dd427a7cc1fd0dd23f76769b75eb6ebd3f6d
MOhammed Zakaria <kokoriquaye@gmail.com>
2023-01-24 22:30:10 +0000

    defines a square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Square module"""
+
+
+class Square:
+    """Defines a square"""
+    def __init__(self, size):
+        """Initialize the square with a size.
+
+        Args:
+            size: size of square
+        """
+        self.__size = size
+
+    pass
