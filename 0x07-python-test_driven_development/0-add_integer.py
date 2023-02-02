618d37945f6425d67dd8024d2ebea1d3b1e25c9d
MOhammed Zakaria <kokoriquaye@gmail.com>
2023-02-02 14:16:14 +0000

    fix

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -5,7 +5,7 @@
 
 def add_integer(a, b=98):
     """Adds two integers.
-    
+
     Args:
         a (int|float): first integer
         b (:obj:`int|float`, optional): second integer. Defaults to 98.
