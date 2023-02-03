59a8c0981ec47c43919c40e07367bdcae4a8d760
MOhammed Zakaria <kokoriquaye@gmail.com>
2023-02-03 18:15:02 +0000

    print text

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Text Indentation module"""
+
+
+def text_indentation(text):
+    """Prints text with 2 new lines after each of these characters
+    ., ? and :
+
+    Args:
+        text: text to print
+
+    Raises:
+        TypeError: text must be a string
+    """
+
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    i = 0
+    while i < (len(text)):
+        if text[i] == "." or text[i] == "?" or text[i] == ":":
+            print(text[i])
+            print()
+            i += 2
+            continue
+        print(text[i], end="")
+        i += 1
+    print()
