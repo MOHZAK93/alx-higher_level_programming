f3e435990683216949b6bd519a51c33df2405883
MOhammed Zakaria <kokoriquaye@gmail.com>
2023-02-03 09:04:52 +0000

    say my name

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""Print Name module"""
+
+
+def say_my_name(first_name, last_name=""):
+    """Prints first_name and last_name
+
+    Args:
+        first_name: first parameter
+        last_name: second parameter
+
+    Raises:
+        TypeError: first_name must be a string
+        TypeError: last_name must be a string
+    """
+
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
