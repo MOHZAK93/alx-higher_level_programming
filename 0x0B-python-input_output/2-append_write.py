2f7e8b1065e6f006a3145451b5e2eb1eb6aa7092
MOhammed Zakaria <kokoriquaye@gmail.com>
2023-02-07 21:27:33 +0000

    append to file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""Append module"""
+
+
+def append_write(filename="", text=""):
+    """Appends a string at the end of a text file (UTF8) and
+        returns the number of characters added
+
+        Args:
+            filename: name of file to append to
+            text: string to append
+
+        Returns:
+            number of characters added
+    """
+    with open(filename, 'a', encoding="utf-8") as f:
+        return f.write(text)
