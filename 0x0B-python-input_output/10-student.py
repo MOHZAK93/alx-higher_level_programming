e6cce32ab8a3ce879b497caf104a8948f9d93ac8
MOhammed Zakaria <kokoriquaye@gmail.com>
2023-02-08 22:22:40 +0000

    student to json with filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Student to JSON module"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a json representation of a Student Instance"""
+        dcopy = self.__dict__.copy()
+        if type(attrs) is list:
+            for attr in attrs:
+                if type(attr) != str:
+                    return dcopy
+            dattrs = {}
+            for attr in attrs:
+                if attr in dcopy:
+                    dattrs[attr] = dcopy[attr]
+            return dattrs
+        return dcopy
