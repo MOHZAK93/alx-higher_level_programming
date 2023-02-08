607298b667de600c8ca5a5fa96b6c3d3db2a0e72
MOhammed Zakaria <kokoriquaye@gmail.com>
2023-02-08 17:23:38 +0000

    fix

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,10 +5,10 @@
 if __name__ == "__main__":
     import json
     import sys
-    load_from_json_file = __import__("6-load_from_json_file")
-    .load_from_json_file
-    save_to_json_file = __import__("5-save_to_json_file")
-    .save_to_json_file
+    load_from_json_file = __import__(
+            "6-load_from_json_file").load_from_json_file
+    save_to_json_file = __import__(
+            "5-save_to_json_file").save_to_json_file
 
     try:
         item_list = load_from_json_file("add_item.json")
