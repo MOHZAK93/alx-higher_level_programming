6fd10840a22b6d5d23c66040279e781b7da9647d
MOhammed Zakaria <kokoriquaye@gmail.com>
2023-02-09 00:00:26 +0000

    square 2

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Square module"""
+
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """Defines a square"""
+
+    def __init__(self, size):
+        """Initializes a square size
+
+        Args:
+            size: size of square
+        """
+        self.__size = size
+        self.integer_validator("size", size)
+
+    def area(self):
+        return self.__size ** 2
+
+    def __str__(self):
+        return "[Square] {:d}/{:d}".format(self.__size, self.__size)
