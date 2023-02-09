f63f45c49ef4820d42d67f3593a20fee3cc4999c
MOhammed Zakaria <kokoriquaye@gmail.com>
2023-02-09 17:23:19 +0000

    can I

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Add attribute module"""
+
+
+def add_attribute(obj, name, value):
+    """Adds a new attribute to an object if it is possible"""
+    try:
+        obj.name = value
+    except:
+        raise TypeError("can't add new attribute")
