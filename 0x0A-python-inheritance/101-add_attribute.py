dcd03cbf39fe672ece1e0f002468171701ae88f2
MOhammed Zakaria <kokoriquaye@gmail.com>
2023-02-09 18:25:16 +0000

    fix

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -4,7 +4,6 @@
 
 def add_attribute(obj, name, value):
     """Adds a new attribute to an object if it is possible"""
-    try:
-        obj.name = value
-    except Exception:
+    if "__dict__" not in dir(obj):
         raise TypeError("can't add new attribute")
+    obj.__setattr__(name, value)
