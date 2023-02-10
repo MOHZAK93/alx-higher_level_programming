28e95a94c1879c32394575d7e70198a8b826fb4f
MOhammed Zakaria <kokoriquaye@gmail.com>
2023-02-10 20:29:36 +0000

    fix

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,24 +1,22 @@
 #!/usr/bin/python3
-
-"""Addition module"""
+"""
+Addition module
+"""
 
 
 def add_integer(a, b=98):
-    """Adds two integers.
-
-    Args:
+    """Adds two integers
+    args:
         a (int|float): first integer
-        b (:obj:`int|float`, optional): second integer. Defaults to 98.
-
+        b (:obj:`int|float`, optional): second integer. Defaults to 98
     Returns:
-        The sum of the two integers as a whole number.
-
+        The sum of the two integers as a whole number
     Raises:
-        TypeError: If `a` or `b` is not int or float.
+        TypeError: If `a` or `b` is not int or float
     """
-    if type(a) not in (int, float):
+    if type(a) not in [int, float] or a in (float('inf'), -float('inf')):
         raise TypeError("a must be an integer")
-    if type(b) not in (int, float):
+    if type(b) not in [int, float] or b in (float('inf'), -float('inf')):
         raise TypeError("b must be an integer")
 
     return int(a) + int(b)
