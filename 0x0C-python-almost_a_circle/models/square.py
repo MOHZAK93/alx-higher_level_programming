16a2d5851b2e6dc046df0d4112fdd514bb0ca017
MOhammed Zakaria <kokoriquaye@gmail.com>
2023-02-13 23:19:38 +0000

    square documentation

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -4,18 +4,33 @@ from .rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """Defines a Square"""
+    """Defines a Square
+
+        Args:
+            Rectangle: parent class to square
+    """
 
     def __init__(self, size, x=0, y=0, id=None):
-        """Initializes Square"""
+        """Initializes Square
+
+        Args:
+                size: size of square
+                x: horizontal offset
+                y: vertical offset
+                id: id of square
+        """
         super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
+        """getter for size of square"""
+
         return self.width
 
     @size.setter
     def size(self, value):
+        """Setter for size of square"""
+
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
@@ -24,7 +39,12 @@ class Square(Rectangle):
         self.height = value
 
     def update(self, *args, **kwargs):
-        """updates square object arguments"""
+        """updates square object arguments
+
+        Args:
+                args: non-keyword argument
+                kwargs: keyword argument
+        """
         if kwargs is not None:
             for key, value in kwargs.items():
                 if key == "id":
@@ -48,10 +68,14 @@ class Square(Rectangle):
                     self.y = args[i]
 
     def __str__(self):
+        """String representation of Square"""
+
         return "[Square] ({:d}) {:d}/{:d} - {:d}".format(
                 self.id, self.x, self.y, self.width)
 
     def to_dictionary(self):
+        """Dictionary representation of Square"""
+
         return {"id": self.id,
                 "size": self.size,
                 "x": self.x,
