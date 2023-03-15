aa5b757aacc44823bb8134bd61c2a2d3c1528829
MOhammed Zakaria <kokoriquaye@gmail.com>
2023-03-15 13:17:14 +0000

    count

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- lists the number of records with the same score in table in MySQL server
-SELECT score, COUNT(score) AS number FROM second_table ORDER BY number DESC;
+SELECT score, COUNT(*) AS number FROM second_table ORDER BY number DESC;
