2812d8d236f57854630b8c830e2a899c299c3cb8
MOhammed Zakaria <kokoriquaye@gmail.com>
2023-03-15 10:38:56 +0000

    say my name

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,5 @@
 -- lists all records of the table without the value name in MySQL server
-SELECT score, name FROM second_table ORDER BY score DESC;
+SELECT score, name
+FROM second_table
+WHERE name IS NOT NULL
+ORDER BY score DESC;
