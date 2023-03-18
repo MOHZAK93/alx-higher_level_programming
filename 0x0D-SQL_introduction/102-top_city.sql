b2baefd94bb024ff3746eaaf204a793ccfc1cdbb
MOhammed Zakaria <kokoriquaye@gmail.com>
2023-03-18 00:02:29 +0000

    Go to UTF8

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,6 +1,6 @@
 -- Displays the top 3 of cities temperature during July and August
 SELECT city, AVG(value) AS avg_temp
 	FROM temperatures
-	GROUP BY cities
+	GROUP BY city
 	LIMIT 3
 ORDER BY state;
