cb192499b9ad7974c29f383a677c3e0b07a7044f
MOhammed Zakaria <kokoriquaye@gmail.com>
2023-03-18 08:07:46 +0000

    temperature

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,2 +1,5 @@
--- displays the average temperature by city oredered by temperature in MySQL server
-SELECT city, AVG(value) AS avg_temp FROM `temperatures` ORDER BY city DESC;
+-- Displays the average temperature (Fahrenheit) by city ordered by temperature
+SELECT city, AVG(value) AS avg_temp
+	FROM temperatures
+	GROUP BY city
+ORDER BY avg_temp DESC;
