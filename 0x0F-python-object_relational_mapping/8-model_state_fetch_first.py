99a027442654dcf57974210d2a14b5cbd9f8d167
MOhammed Zakaria <kokoriquaye@gmail.com>
2023-04-16 11:32:08 +0000

    first state

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Prints the first State object from the database hbtn_0e_6_usa"""
+
+import sys
+from model_state import Base, State
+
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session
+
+
+if __name__ == "__main__":
+    """Make a connection to database"""
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3], poop_pre_ping=True))
+
+    """Create a session object"""
+    session = Session(engine)
+
+    states = session.query(State).filter(State.id == 1).all()
+
+    if states:
+        for state in states:
+            print("{}: {}".format(state.id, state.name))
+        session.close()
+    else:
+        print("Nothing")
