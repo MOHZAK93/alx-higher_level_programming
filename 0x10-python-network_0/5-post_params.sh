921313b4d66e3c88e30b12c20f08ad432b6018ca
MOhammed Zakaria <kokoriquaye@gmail.com>
2023-04-28 21:18:04 +0000

    cURL POST parameters

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#Sends a GET request and displays body of the response
+#Sends a POST request to the passed URL and displays the body of the response
 curl -sLX POST -d 'email=test@gmail.com&subject=I+will+always+be+here+for+PLD' "$1"
