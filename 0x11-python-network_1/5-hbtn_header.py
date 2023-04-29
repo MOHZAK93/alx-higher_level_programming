e326dd4a64ac67daad845c202eab99a7b2a975fb
MOhammed Zakaria <kokoriquaye@gmail.com>
2023-04-29 15:36:32 +0000

    response header value

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -7,5 +7,5 @@ import sys
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         res = rq.get(sys.argv[1])
-        if 'X-Request-Id' in response.headers:
+        if 'X-Request-Id' in res.headers:
             print(res.headers['X-Request-Id'])
