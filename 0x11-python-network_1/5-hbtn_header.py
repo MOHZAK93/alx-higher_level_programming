b6b8f248bb18fb9ff06c9ea14bc09593fb46b665
MOhammed Zakaria <kokoriquaye@gmail.com>
2023-04-29 15:33:15 +0000

    response header value

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Displays the value of th variable X-Request-Id"""
+import requests as rq
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        res = rq.get(sys.argv[1])
+        if 'X-Request-Id' in response.headers:
+            print(res.headers['X-Request-Id'])
