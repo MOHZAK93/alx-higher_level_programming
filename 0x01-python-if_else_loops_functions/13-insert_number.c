4808668d768b67caed745d1b985f331710ce313f
MOhammed Zakaria <kokoriquaye@gmail.com>
2023-04-30 17:50:40 +0000

    insert in sorted linked list

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -19,6 +19,12 @@ listint_t *insert_node(listint_t **head, int number)
 	if (!new_node)
 		return (NULL);
 	new_node->n = number;
+
+	if (!*head)
+	{
+		*head = new_node;
+		return (new_node);
+	}
 	if ((*head)->n > new_node->n)
 	{
 		new_node->next = *head;
