b238a57c17ffe168b1d46aa5acadf085cfdb12b5
MOhammed Zakaria <kokoriquaye@gmail.com>
2023-05-05 13:41:05 +0000

    search API

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Search API"""
+import requests as rq
+import sys
+
+
+if __name__ == "__main__":
+    url = 'http://0.0.0.0:5000/search_user'
+    if len(sys.argv) > 1:
+        q = {'q': sys.argv[1]}
+    else:
+        q = {'q': ''}
+    res = rq.post(url, data=q)
+    try:
+        obj = res.json()
+        if len(obj) == 0:
+            print(f"No result")
+        else:
+            print(f"[{boj['id']}] {obj['name']}")
+    except ValueError:
+        print(f"Not a valid JSON")
