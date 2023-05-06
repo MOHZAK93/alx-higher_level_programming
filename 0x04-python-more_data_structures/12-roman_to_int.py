e6a604bed050742cefeff5b6fec96471787b4e56
MOhammed Zakaria <kokoriquaye@gmail.com>
2023-05-06 21:40:14 +0000

    roman integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,50 +1,12 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    result = 0
     value = 0
-    for y in range(len(roman_string)):
-        if y == 0:
-            value += dic[roman_string[y]]
-        if roman_string[y] == 'I' and y != 0:
-            if dic['I'] <= dic[roman_string[y - 1]]:
-                value += dic['I']
-            else:
-                value += dic['I']
-                value -= dic[roman_string[y - 1]]
-        elif roman_string[y] == 'V' and y != 0:
-            if dic['V'] <= dic[roman_string[y - 1]]:
-                value += dic['V']
-            else:
-                value += dic['V']
-                value -= dic[roman_string[y - 1]]
-        elif roman_string[y] == 'X' and y != 0:
-            if dic['X'] <= dic[roman_string[y - 1]]:
-                value += dic['X']
-            else:
-                value += dic['X']
-                value -= dic[roman_string[y - 1]]
-        elif roman_string[y] == 'L' and y != 0:
-            if dic['L'] <= dic[roman_string[y - 1]]:
-                value += dic['L']
-            else:
-                value += dic['L']
-                value -= dic[roman_string[y - 1]]
-        elif roman_string[y] == 'C' and y != 0:
-            if dic['C'] <= dic[roman_string[y - 1]]:
-                value += dic['C']
-            else:
-                value += dic['C']
-                value -= dic[roman_string[y - 1]]
-        elif roman_string[y] == 'D' and y != 0:
-            if dic['D'] <= dic[roman_string[y - 1]]:
-                value += dic['D']
-            else:
-                value += dic['D']
-                value -= dic['D']
-        elif roman_string[y] == 'M' and y != 0:
-            if dic['M'] <= dic[roman_string[y - 1]]:
-                value += dic['M']
-            else:
-                value += dic['M']
-                value -= dic[roman_string[y - 1]]
-    return (value)
+    for i in range(len(roman_string) - 1, -1, -1):
+        value = dic[roman_string[i]]
+        if i < len(roman_string) - 1 and dic[roman_string[i + 1]] > value:
+            result -= value
+        else:
+            result += value
+    return (result)
