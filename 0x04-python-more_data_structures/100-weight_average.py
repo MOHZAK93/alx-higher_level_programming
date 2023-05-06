4d2870e5b0996f9f5cc39d86614e068e7dcf49ef
MOhammed Zakaria <kokoriquaye@gmail.com>
2023-05-06 23:20:44 +0000

    weighted average

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 def weight_average(my_list=[]):
-    if my_list is None:
+    if my_list == []:
         return (0)
     return (sum([i[0] * i[1] for i in my_list]) / sum([j[1] for j in my_list]))
