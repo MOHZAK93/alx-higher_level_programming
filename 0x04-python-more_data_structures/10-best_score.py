4cb07a5aafcfed6ff4fd8176d00129c46197a0ec
MOhammed Zakaria <kokoriquaye@gmail.com>
2023-05-06 20:07:01 +0000

    fix

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     biggest = 0
-    if a_dictionary == None:
+    if a_dictionary is None:
         return (None)
     for key, value in a_dictionary.items():
         if biggest < value:
