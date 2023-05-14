307733261f440c312addba15334899bc384c53fc
MOhammed Zakaria <kokoriquaye@gmail.com>
2023-05-14 14:21:03 +0000

    fix

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,6 +4,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError:
-        print("Exception: ", file=sys.stderr)
+    except ValueError as e:
+        print(f"Exception: {e}")
         return False
