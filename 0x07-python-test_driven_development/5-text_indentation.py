155d0e67966af1107802d26e7e1a530b0bc72496
MOhammed Zakaria <kokoriquaye@gmail.com>
2023-06-04 07:37:32 +0000

    update

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -26,7 +26,7 @@ def text_indentation(text):
             if text[i] in ".?:":
                 print("\n")
             i += 1
-            while i < len(text) and text[i]== " ":
+            while i < len(text) and text[i] == " ":
                 i += 1
             continue
         i += 1
