dbda90da876907122c2aed46d19bd7afafeeb1a8
MOhammed Zakaria <kokoriquaye@gmail.com>
2023-06-04 17:06:57 +0000

    update

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Matrix multiplication using numpy module"""
+
+
+import numpy as np
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """Multiplies 2 matrices"""
+
+    if type(m_a) is not list:
+        raise TypeError("m_a must be a list")
+    if type(m_b) is not list:
+        raise TypeError("m_b must be a list")
+    if not all(isinstance(row, list) for row in m_a):
+        raise TypeError("m_a must be a list of lists")
+    if not all(isinstance(row, list) for row in m_b):
+        raise TypeError("m_b must be a list of lists")
+    if m_a == [] or m_a == [[]]:
+        raise ValueError("m_a can't be empty")
+    if m_b == [] or m_b == [[]]:
+        raise ValueError("m_b can't be empty")
+    if any(type(elem) not in [int, float]
+           for elem in [x for row in m_a for x in row]):
+        raise TypeError("m_a should contain only integers or floats")
+    if any(type(elem) not in [int, float]
+           for elem in [x for row in m_b for x in row]):
+        raise TypeError("m_b should contain only integers or floats")
+    if any(len(row) != len(m_a[0]) for row in m_a):
+        raise TypeError("each row of m_a must be of the same size")
+    if any(len(row) != len(m_b[0]) for row in m_b):
+        raise TypeError("each row of m_b must be of the same size")
+    if len(m_a[0]) != len(m_b):
+        raise ValueError("m_a and m_b can't be multiplied")
+
+    return np.dot(m_a, m_b)
